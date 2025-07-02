be42b3931d04a525c009714ad32d5cc842251135
Davo <warhead_1090@hotmail.com>
2025-07-01 18:35:07 -0600

    Starting with Databases.
    - added .db to gitignore
    - created models file with a first model for
    the todo list.
    - created main for todo app, where logic for
    actual endpoint goes.

---
+++ TodoApp/main.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import models
+from models import Todos
+from database import SessionLocal, engine
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
