a97fada2ac9b054016c35fb625be38fa89eff78c
David del Real <warhead_1090@hotmail.com>
2025-07-14 18:55:35 -0600

    Added auth file

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -4,10 +4,13 @@ from sqlalchemy.orm import Session
 import models
 from models import Todos
 from database import SessionLocal, engine
+from routers import auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
 
 def get_db():
     db = SessionLocal()
