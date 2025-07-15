41ddb633d0e3d631204b248fa1efb965d2c3dac3
David del Real <warhead_1090@hotmail.com>
2025-07-14 21:04:18 -0600

    Separated main into route for todo list. created model for user and auth stuff. added endpoints pot auth.

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,7 +1,60 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {'user', 'authenticated'}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def authenticate_user(username: str, password: str, db: Session):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password): # type: ignore
+        return False
+    return True
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str 
+    last_name: str
+    password: str
+    role: str
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def get_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password ),
+        is_active=True
+    )
+
+    db.add(create_user_model)
+    db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication: you dont exist m8.'
+    return 'Successful Authentication: please do come in.'
