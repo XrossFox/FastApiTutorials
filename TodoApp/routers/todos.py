f3ffe1be47c8fc9ea752a538019f0cb659eee466
David del Real <warhead_1090@hotmail.com>
2025-10-18 06:27:54 -0600

    Added Tests WIP
    Added suppot for PyTest
    Created database for testing
    Created default test env conf file
    Created first examples for testing modules
    Created utils for tests>

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -74,7 +74,7 @@ async def update_todo(user: user_dependency, db: db_dependency,
     db.add(todo_model)
     db.commit()
 
-@router.delete("/todo/{todo_id}")
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication failed")
---
+++ TodoApp/test/test_todos.py
@@ -0,0 +1,138 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from database import Base
+from main import app
+from routers.todos import get_db, get_current_user
+from fastapi.testclient import TestClient
+from fastapi import status
+from models import Todos
+import pytest
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def override_get_current_user():
+    return {"username": "ambrosio", "id": 1, "user_role": "admin"}
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+client = TestClient(app)
+
+@pytest.fixture
+def test_todo():
+    todo: Todos = Todos(
+        title="Some title",
+        description="A description",
+        priority=5,
+        complete=False,
+        owner_id=1
+    )
+
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM todos;"))
+        connection.commit()
+
+def test_read_all_authenticated(test_todo):
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    for_assert = [{
+        "description": "A description",
+        "complete": False,
+        "title": "Some title",
+        "priority": "5",
+        "owner_id": 1,
+        "id": 1
+    }]
+    assert response.json() == for_assert
+
+def test_read_one_authenticated(test_todo):
+    response = client.get("/todo/1")
+    assert response.status_code == status.HTTP_200_OK
+    for_assert = {
+        "description": "A description",
+        "complete": False,
+        "title": "Some title",
+        "priority": "5",
+        "owner_id": 1,
+        "id": 1
+    }
+    assert response.json() == for_assert
+
+def test_read_one_authenticated_not_found():
+    response = client.get("/todo/999")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Todo not found."}
+
+def test_create_todo(test_todo):
+    request_data = {
+        "title": "New Todo",
+        "description": "New description",
+        "priority": 5,
+        "complete": False,
+    }
+
+    response = client.post("/todo/", json=request_data)
+    assert response.status_code == 201 
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id==2).first()
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert int(model.priority) == request_data.get("priority")
+    assert model.complete == request_data.get("complete")
+
+def test_update_todo(test_todo):
+    request_data={
+        "title": "Changed title of todo",
+        "description": "Changed description",
+        "priority": 4,
+        "complete": True
+    }
+
+    response = client.put("/todo/1", json=request_data)
+    assert response.status_code == 204
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id==1).first()
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert int(model.priority) == request_data.get("priority")
+    assert model.complete == request_data.get("complete")
+
+def test_update_todo_not_found(test_todo):
+    request_data={
+        "title": "Changed title of todo",
+        "description": "Changed description",
+        "priority": 4,
+        "complete": True
+    }
+
+    response = client.put("/todo/999", json=request_data)
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Todo not found"}
+
+def test_delete_todo_not_found(test_todo):
+    response = client.delete("/todo/999")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Todo not found"}
